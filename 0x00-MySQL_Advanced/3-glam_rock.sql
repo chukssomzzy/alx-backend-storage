141547987b2ec4583de40c1207b05930e6110c6f
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-18 17:06:00 +0000

    if null

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,14 +3,10 @@
 -- SELECT WHERE 
 SELECT
 band_name,
-(split - formed) AS lifespan, 
+(IFNULL(split, 2022) - formed) AS lifespan, 
 FROM
 metal_bands 
 WHERE
-AND
-split
 style LIKE '%glam rock%' 
-AND 
-split <= 2022
 ORDER BY 
 lifespan DESC;
