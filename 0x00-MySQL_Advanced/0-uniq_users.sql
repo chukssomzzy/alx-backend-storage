13a33f6fd7560e1a77636af3b57e8c80c6cb4983
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-18 14:14:23 +0000

    created wrong table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Create users with unique column 
 
 -- Create a users Table 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
