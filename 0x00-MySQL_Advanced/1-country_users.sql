cfe3b6966ff16e75ae6dbec08034e138c031e615
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-18 14:46:57 +0000

    duplicate column name

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,8 +2,8 @@
 
 -- users table 
 CREATE TABLE IF NOT EXISTS users (
-  name INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
   name VARCHAR(255),
-  country ENUM('US', 'CO', 'TN')
+  country ENUM('US', 'CO', 'TN') DEFAULT country+1
 )
