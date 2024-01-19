1e7053260924dafa71d9a7f882f9f60c220202a4
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-19 18:05:59 +0000

    delimeter

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -5,11 +5,9 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-  DECLARE udiv FLOAT;
   IF b = 0 or ISNULL(b)
     THEN
     RETURN 0;
-  END IF
-  SET udiv = (a / b);
-  RETURN udiv;
+  END IF;
+  RETURN (a / b);
 END$$
