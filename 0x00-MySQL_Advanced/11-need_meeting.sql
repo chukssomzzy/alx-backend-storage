c092bcccd6c273856cfb86589d9bb4fea4cccf55
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-19 22:40:42 +0100

    less than or equal to 80

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,7 +4,7 @@
 CREATE OR REPLACE VIEW need_meeting
 AS
 SELECT name FROM students 
-WHERE score < 80 
+WHERE score <= 80 
 AND 
 (ISNULL(last_meeting) 
   OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH));
