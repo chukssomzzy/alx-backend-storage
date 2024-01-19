38b1dd34c921f922974e1f3d2fc33e36ced455a5
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-19 06:52:15 +0100

    complain about a return

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,11 +5,10 @@ DELIMITER $$
 CREATE TRIGGER isValidEmail
 AFTER UPDATE ON users 
 FOR EACH ROW 
-  BEGIN
-    SELECT OLD.email, NEW.email;
-    IF NEW.email AND OLD.email != NEW.email
-      THEN
-      UPDATE users SET valid_email = 0
-      WHERE email = NEW.email;
-    END IF;
-  END$$
+BEGIN
+  IF NEW.email AND OLD.email != NEW.email
+    THEN
+    UPDATE users SET valid_email = 0
+    WHERE email = NEW.email;
+  END IF;
+END$$
