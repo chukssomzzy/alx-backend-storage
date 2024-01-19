649d78cea8e38fc6b2d123c9e788252f5d48ff7d
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-19 07:09:07 +0100

    incorrect double value

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,12 +3,10 @@
 -- Is valid email 
 DELIMITER $$
 CREATE TRIGGER isValidEmail
-BEFORE UPDATE ON users 
+AFTER UPDATE ON users 
 FOR EACH ROW 
 BEGIN
-  SELECT * FROM NEW;
-  SELECT * FROM OLD;
-  IF NEW.email AND OLD.email != NEW.email
+  IF OLD.email != NEW.email
     THEN
     UPDATE users SET valid_email = 0
     WHERE email = NEW.email;
