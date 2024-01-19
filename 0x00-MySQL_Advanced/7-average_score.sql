be0b5905f5a365d9fe52cf231a874062ff8d1f5d
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-19 08:41:06 +0100

    test

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- Compute Average Score 
+
+--  Average Score For user 
+CREATE PROCEDURE computeAverageScoreForUser(IN user_id INT)
+LANGUAGE SQL
+COMMENT 'Average corrections score' 
+CONTAINS SQL 
+NOT DETERMINISTIC 
+SQL SECURITY DEFINER 
+BEGIN 
+  DECLARE u_avg_score FLOAT; 
+
+  SELECT AVG(score) INTO u_avg_score 
+  FROM corrections 
+  WHERE corrections.user_id = user_id;
+
+  UPDATE users 
+  SET average_score = u_avg_score
+  WHERE id = user_id; 
+END;
