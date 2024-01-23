3111535861df8c9658c172524fce59be1fa7ca8f
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-23 07:10:48 +0100

    removed anottation

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Update Topic"""
-from typing import Any, List
 
 
-def update_topics(mongo_collection: Any, name: str, topics: List[str]) -> None:
+def update_topics(mongo_collection, name, topics) -> None:
     """Update collection topic by name"""
     mongo_collection.update({"name": name},
                             {"$set": {"topics": topics}})
