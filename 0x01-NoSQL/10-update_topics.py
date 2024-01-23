556c8094c6af3a3ab15580f5bb7b0bda65947052
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-23 07:14:33 +0100

    no

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,7 +2,14 @@
 """Update Topic"""
 
 
+from typing import ParamSpecKwargs
+
+
 def update_topics(mongo_collection, name, topics) -> None:
     """Update collection topic by name"""
     mongo_collection.update({"name": name},
                             {"$set": {"topics": topics}})
+
+
+if __name__ == "__main__":
+    pass
