a5a6961e0fdae5edc61f8756af9d8620a0a0261e
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-24 16:51:03 +0000

    local redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,35 +10,21 @@ from redis.commands.core import ResponseT
 
 class Cache:
     """Redis cache implementation"""
-    data_types = [str, float, int, bytes]
+    data_types = [str.__name__, float.__name__, int.__name__, bytes.__name__]
 
     def __init__(self) -> None:
         """setup cache"""
         self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, int, bytes]) -> str:
+    def store(self, data: Union[str, float, int, bytes]) -> Union[str, None]:
         """takes a data and return a key
         args:
             data (str|float|int|bytes): value to store in redis database
         """
-        key = ""
-        if type(data) in self.data_types:
+
+        if data and type(data).__name__ in self.data_types:
             key = str(uuid.uuid4())
             self._redis.set(key, data)
-        return key
+            return key
 
-    def get(self, key: str, fn: Callable) -> Any:
-        """get an item from redis cache"""
-        value: ResponseT = self._redis.get(key)
-        if value:
-            value = fn(value)
-        return value
-
-    def get_int(self, key: str) -> int:
-        """Paramitarize Cache.get with the correct function"""
-        return self.get(key, int)
-
-    def get_str(self, key: str) -> str:
-        """Paramitarize Cache.get with the correct convertion function """
-        return self.get(key, str)
