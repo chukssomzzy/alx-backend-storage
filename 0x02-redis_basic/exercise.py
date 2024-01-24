6f274995df51d7045be7b7e45d2ee8e08acac97f
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-24 16:45:55 +0100

    added more documentation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""redis setup"""
+"""redis Cache class"""
 
 from typing import Union
 import redis
@@ -7,7 +7,7 @@ import uuid
 
 
 class Cache:
-    """Redis cache"""
+    """Redis cache implementation"""
     data_types = [str, float, int, bytes]
 
     def __init__(self) -> None:
@@ -16,7 +16,10 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, float, int, bytes]) -> str:
-        """takes a data and return a key"""
+        """takes a data and return a key
+        args:
+            data (str|float|int|bytes): value to store in redis database
+        """
         key = ""
         if type(data) in self.data_types:
             key = str(uuid.uuid4())
