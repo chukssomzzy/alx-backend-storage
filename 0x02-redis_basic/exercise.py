d7c6845c2c25a05f2f1ebd8b3930b8623d13871d
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-24 16:53:39 +0000

    datatypes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
 """redis Cache class"""
 
-from typing import Any, Callable, Union
+from typing import Union
 import redis
 import uuid
-
-from redis.commands.core import ResponseT
 
 
 class Cache:
     """Redis cache implementation"""
-    data_types = [str.__name__, float.__name__, int.__name__, bytes.__name__]
+    data_types = [str, float, int, bytes]
 
     def __init__(self) -> None:
         """setup cache"""
         self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, int, bytes]) -> Union[str, None]:
+    def store(self, data: Union[str, float, int, bytes]) -> str:
         """takes a data and return a key
         args:
             data (str|float|int|bytes): value to store in redis database
         """
-
-        if data and type(data).__name__ in self.data_types:
+        key = ""
+        if type(data) in self.data_types:
             key = str(uuid.uuid4())
             self._redis.set(key, data)
-            return key
-
+        return key
