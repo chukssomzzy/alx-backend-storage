213521f1aed23c5730b05031541f2cec7fa6af88
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-24 16:56:22 +0000

    set a value

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,20 +8,17 @@ import uuid
 
 class Cache:
     """Redis cache implementation"""
-    data_types = [str, float, int, bytes]
 
     def __init__(self) -> None:
         """setup cache"""
         self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, int, bytes]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data and return a key
         args:
             data (str|float|int|bytes): value to store in redis database
         """
-        key = ""
-        if type(data) in self.data_types:
-            key = str(uuid.uuid4())
-            self._redis.set(key, data)
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
         return key
