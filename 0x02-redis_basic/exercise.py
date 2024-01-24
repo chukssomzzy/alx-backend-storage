eb47f2521111f7df9fc4c2faa9704ecb30f8a7e9
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-24 16:27:53 +0100

    setup cache

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""redis setup"""
+
+from typing import Union
+import redis
+import uuid
+
+
+class Cache:
+    """Redis cache"""
+    data_types = [str, float, int, bytes]
+
+    def __init__(self):
+        """setup cache"""
+        self._redis: redis.Redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, float, int, bytes]) -> str:
+        """takes a data and return a key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
