c4b0fd67db63f20432db482f2a4aa3458dc9f759
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-25 01:06:56 +0100

    diff

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -38,22 +38,22 @@ def call_history(f: Callable) -> Callable:
 
 
 class Cache:
-    '''Redis cache implementation.
+    '''Represents an object for storing data in a Redis data storage.
     '''
     def __init__(self) -> None:
-        '''setup cache instance
+        '''Initializes a Cache instance.
         '''
-        self._redis: redis.Redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''takes a data and return a key  value to store in redis database.
+        '''Stores a value in a Redis data storage and returns the key.
         '''
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
 
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
