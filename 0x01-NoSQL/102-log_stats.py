28656e2b4f37cc39d6f95a51d5945049111f8dd9
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-27 00:39:03 +0100

    ips count and top ten print

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""improved logs with aggregate"""
+
+from pymongo import MongoClient
+
+
+def top_ten(col):
+    """get top ten ips in col"""
+    res = col.aggregate([
+        {
+            "$group": {
+                "_id": "$ip",
+                "appeared": {
+                    "$count": {}
+                }
+            }
+        },
+        {
+            "$sort": {
+                "appeared": -1
+            }
+        },
+        {
+            "$limit": 10
+        }
+    ])
+    return res
+
+
+def main():
+    """Start of program execution"""
+    client = MongoClient("mongodb://localhost:27017")
+    nginx = client.logs.nginx
+
+    print(f"{nginx.count_documents({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {nginx.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {nginx.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {nginx.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {nginx.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {nginx.count_documents({'method': 'DELETE'})}")
+    sum = nginx.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{sum} status check")
+    print("IPs:")
+    for doc in top_ten(nginx):
+        print("\t{}: {}".format(doc["_id"], doc["appeared"]))
+
+
+if __name__ == "__main__":
+    main()
